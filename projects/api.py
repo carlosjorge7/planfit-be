f5c7b14643664cf6b12e607f2d8b2476f5805670
Carlos <carlos_jorge@fremap.es>
2023-09-18 19:28:15 +0200

    wip

---
+++ projects/api.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, permissions
+
+from .models import Project
+from .serializers import (
+    ProjectSerializer,
+   
+)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ProjectSerializer
