be37e823b2da6f9f4ade452c1d3be43c44886f1a
Carlos <carlos_jorge@fremap.es>
2023-11-29 13:12:26 +0100

    root access users

--- projects/api.py
+++ projects/api.py
@@ -16,9 +16,32 @@ from django.db.models import Q
 
 
 # USER API
-class UserCreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
+class UserCreateView(generics.ListCreateAPIView):
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        root_param = self.request.query_params.get("root")
+
+        # Verificar si se proporcionó el parámetro 'root' con el valor '1234'
+        if root_param == "planfitRootAdmin232":
+            # Si es así, mostrar la lista de usuarios
+            return User.objects.all()
+
+        # Si no se proporciona el parámetro o no tiene el valor esperado, retornar una lista vacía
+        return User.objects.none()
+
+    def perform_create(self, serializer):
+        # La creación de usuarios es libre, no hay condiciones
+        serializer.save()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        # Serializar los usuarios
+        serializer = self.serializer_class(queryset, many=True)
+
+        # Devolver la respuesta
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class UserRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
